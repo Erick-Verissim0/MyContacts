1106961cd94ff559962dfeb33aca6ce51573862a
Erick_Veríssimo <estudodoerick@gmail.com>
2023-02-11 22:20:55 -0300

    refatoring_find_method

--- src/app/repositories/ContactsRepository.js
+++ src/app/repositories/ContactsRepository.js
@@ -20,14 +20,14 @@ let contacts = [
 ];
 
 class ContactsRepository {
-  findAll() {
-    return new Promise((resolve) => resolve(contacts));
+  async findAll() {
+    const rows = await db.query('SELECT * FROM contacts')
+    return rows;
   }
 
-  findById(id) {
-    return new Promise((resolve) =>
-      resolve(contacts.find((contact) => contact.id === id))
-    );
+  async findById(id) {
+    const [row] = await db.query('SELECT * FROM contacts WHERE id = $1', [id])
+    return row;
   }
 
   delete(id) {
@@ -37,10 +37,9 @@ class ContactsRepository {
     });
   }
 
-  findByEmail(email) {
-    return new Promise((resolve) =>
-      resolve(contacts.find((contact) => contact.email === email))
-    );
+  async findByEmail(email) {
+    const [row] = await db.query('SELECT * FROM contacts WHERE email = $1', [email])
+    return row;
   }
 
   async create({ name, email, phone, category_id }) {
